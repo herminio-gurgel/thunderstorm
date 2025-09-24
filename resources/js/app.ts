23fe33e8faed26964f9ac148ed6a591148eef344
herminio <herminio.gurgel@gmail.com>
2025-09-24 14:07:07 +0000

    :lipstick: style: removendo importações  e estilo global do tailwind

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,6 +1,5 @@
 import 'unfonts.css';
 import 'vuetify/styles';
-import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
