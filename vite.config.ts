534a5bd8e1c7a16061330df4df7185a122bfac40
herminio <herminio.gurgel@gmail.com>
2025-09-25 13:00:21 +0000

    :hammer: test: configuração básica do `vitest` para testes no frontend

--- vite.config.ts
+++ vite.config.ts
@@ -36,4 +36,14 @@ export default defineConfig({
             },
         }),
     ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './resources/js/tests/setup.ts',
+        server: {
+            deps: {
+                inline: ['vuetify'],
+            },
+        },
+    },
 });
