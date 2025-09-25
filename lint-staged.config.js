f43b4a78d7a4907f0d04b9848c82c1ce45dfb06d
herminio <herminio.gurgel@gmail.com>
2025-09-25 15:32:50 +0000

    :hammer: chore: atualiza os scripts do `lint-staged`
    - removido trecho deprecated no pre-commit do husky
    
    - lint-staged roda testes e checagem de tipos

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 export default {
     '*.{js,ts,vue}': ['eslint --fix', 'prettier --write'],
     '*.{html,css,yaml,md,json}': 'prettier --write',
+    '*.{ts,vue}': 'vitest related --run',
+    '**/*.{ts,vue}': () => 'vue-tsc --build',
 };
