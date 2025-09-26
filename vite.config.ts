62626a8d5af1b02220367d15389bc02c2c06c5ce
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-09-26 13:19:48 +0000

    💚 fix: desfaz a última alteração e altera o comando de run

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         laravel({
             input: ['resources/js/app.ts'],
             ssr: 'resources/js/ssr.ts',
-            refresh: !process.env.CI,
+            refresh: true,
         }),
         tailwindcss(),
         wayfinder({
